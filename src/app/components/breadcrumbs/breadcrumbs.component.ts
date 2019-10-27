60742b905aa7d0ce6aad52f82761ee715ef55004
volha.kireyeva <volha_kireyeva@epam.com>
2019-10-28 00:29:09 +0300

    refactor: unsubscibe for observable, add public modificator to list item

--- src/app/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd, PRIMARY_OUTLET } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 import { map } from 'rxjs/internal/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
-
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
   public breadcrumbs;
   private activatedRoute: ActivatedRoute;
   private router: Router;
@@ -27,7 +28,7 @@ export class BreadcrumbsComponent implements OnInit {
         while (route.firstChild) { route = route.firstChild; }
         return route;
       }))
-      .pipe(filter(route => route.outlet === PRIMARY_OUTLET))
+      .pipe(filter(route => route.outlet === PRIMARY_OUTLET), takeUntil(this.unsubscribe$))
       .subscribe(route => {
         this.breadcrumbs = [];
         const routeData = route.snapshot.data;
@@ -36,5 +37,9 @@ export class BreadcrumbsComponent implements OnInit {
           label
         });
       });
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -7,5 +7,5 @@ import {Component, Input } from '@angular/core';
 })
 export class ListItemComponent {
   @Input()
-  course;
+  public course;
 }
